c68d251c98088fc0bd558b0d87e574bb1ccb5707
Alanoud <alanood94895@gmail.com>
2026-04-24 17:47:29 +0400

    teaParty

--- src/teaParty.java
+++ src/teaParty.java
@@ -1,6 +1,18 @@
 public class teaParty {
     public static void main (String[] args){
-        System.out.println();
-        System.out.println();
+        System.out.println(teaParty(20, 6));
+        System.out.println(teaParty(6, 8));
+    }
+    public static int teaParty(int tea, int candy){
+        if (tea < 5 || candy < 5){
+            return 0;
+        }
+        if (tea >= 5 && candy >= 5){
+            return 1;
+        }
+        if (tea >= 2 * candy || candy >= 2 * tea){
+            return 2;
+        }
+        return 1;
     }
 }
