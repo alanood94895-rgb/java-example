f27fc8518e4810674cc249dd30c59e1aebff2655
Alanoud <alanood94895@gmail.com>
2026-04-25 10:48:42 +0400

    only14

---
+++ src/only14.java
@@ -0,0 +1,14 @@
+public class only14 {
+    public static void main (String[] args){
+        System.out.println();
+        System.out.println();
+    }
+    public static boolean only14 (int []nums){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 1 && nums[i] != 4) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
